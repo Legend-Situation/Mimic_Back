2c9c3adf02da935f03e3f2d702141081aa6c25db
Jamkris <dltmdgus1412@gmail.com>
2024-08-29 08:35:21 +0900

    ADD :: 로그 설정

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 const dotenv = require('dotenv');
 const path = require('path');
+const requestIp = require("request-ip");
 dotenv.config();
 
 app.use(express.json());
@@ -10,8 +11,18 @@ app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname + '/public')));
 
+// Server Logs
+app.use((req, res, next) => {
+	const ipAddress = requestIp.getClientIp(req);
+	const requestPath = req.path;
+	console.log(`-------------------------------------------------------------`);
+	console.log(`Request received from IP: ${ipAddress}, Path: ${requestPath}`);
+	console.log(`-------------------------------------------------------------`);
+	next();
+});
+
 //Port Setting
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
 //API Test
 app.get('/', (req, res) => {
@@ -27,5 +38,6 @@ app.use('/', ApiRouter);
 
 //Port
 db.sequelize.sync().then(() => {
+	console.log(`-------------------------------------------------------------`);
 	app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 });
