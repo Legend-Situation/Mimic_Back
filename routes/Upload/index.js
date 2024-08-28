14ea3800f287bf05b05f153b482c4ab67cfb2808
Jamkris <dltmdgus1412@gmail.com>
2024-08-28 18:46:08 +0900

    ADD :: GPT를 통해서 대화 상대를 가져올 수 있음

--- routes/Upload/index.js
+++ routes/Upload/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const { validateToken } = require('../../middlewares/AuthMiddleware');
 const { imgUpload, chatUpload } = require('./Upload');
+const request = require('request');
+const fs = require('fs');
+const GptBody = require('./GptBody.json')
 
 router.post('/image', validateToken, imgUpload.single('img'), (req, res) => {
 	const IMG_URL = `${process.env.SERVER_ORIGIN}/image/${req.file.filename}`;
@@ -9,8 +12,58 @@ router.post('/image', validateToken, imgUpload.single('img'), (req, res) => {
 });
 
 router.post('/chat', validateToken, chatUpload.single('file'), (req, res) => {
+	const filePath = req.file.path;
 	const FILE_URL = `${process.env.SERVER_ORIGIN}/chat/${req.file.filename}`;
-	res.json({ url: FILE_URL });
+
+	// 파일에서 텍스트 추출
+	fs.readFile(filePath, 'utf8', (err, data) => {
+		if (err) {
+			console.error(err);
+			return res.status(500).send('Failed to read txt');
+		}
+
+		// GPTBody 업데이트
+		const updatedGptBody = {
+			...GptBody,
+			messages: [
+				...GptBody.messages, // 기존 메시지를 유지
+				{   // 새로운 사용자 메시지 추가
+					"role": "user",
+					"content": [
+						{
+							"type": "text",
+							"text": data
+						}
+					]
+				}
+			]
+		};
+
+		// GPT API 요청
+		const options = {
+			uri: 'https://api.openai.com/v1/chat/completions',
+			method: 'POST',
+			body: updatedGptBody,
+			json: true,
+			headers: {
+				'Authorization': `Bearer ${process.env.GPT_SECRET_KEY}`,
+				'Content-Type': 'application/json'
+			},
+		};
+
+		request.post(options, function (err, httpResponse, body) {
+			if (err) {
+				console.error(err);
+				return res.status(500).send('Failed to send GPT request');
+			}
+
+			const contents = body.choices.map(choice => {
+				return JSON.parse(choice.message.content);
+			});
+			res.json({ FileURL: FILE_URL, Contents: contents });
+		});
+	});
 });
+
 
 module.exports = router;
