d5ad3510d0014c78e511e71a4eb81394a505b156
Jamkris <dltmdgus1412@gmail.com>
2024-11-11 17:23:38 +0900

    FIX :: 대화내용에서 사용자 추출

--- routes/Upload/index.js
+++ routes/Upload/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { validateToken } = require('../../middlewares/AuthMiddleware');
 const { imgUpload, chatUpload } = require('./Upload');
+const request = require('request');
 const fs = require('fs');
 const GptBody = require('./GptBody.json');
 const authUtil = require('../../response/authUtil.js');
@@ -26,25 +27,64 @@ router.post('/chat', validateToken, chatUpload.single('file'), (req, res) => {
 				.send(authUtil.successTrue(500, '텍스트파일을 읽을 수 없습니다.', { error: err }));
 		}
 
-		const lines = data.split('\n').slice(1);
-		const cleanedData = lines.map(line => {
-			const columns = line.split(',');
-			return columns.slice(1).join(','); 
-		}).join('\n');
+		const finalResult = Parsing(data);
 
-		const truncatedData = cleanedData.substring(0, 1500);
+		// GPTBody 업데이트
+		const updatedGptBody = {
+			...GptBody,
+			messages: [
+				...GptBody.messages,
+				{
+					"role": "user",
+					"content": [
+						{
+							"type": "text",
+							"text": finalResult
+						}
+					]
+				}
+			]
+		};
 
-		fs.writeFile(filePath.replace('.csv', '.txt'), truncatedData, 'utf8', (err) => {
+		// GPT API 요청
+		const options = {
+			uri: 'https://api.openai.com/v1/chat/completions',
+			method: 'POST',
+			body: updatedGptBody,
+			json: true,
+			headers: {
+				'Authorization': `Bearer ${process.env.GPT_SECRET_KEY}`,
+				'Content-Type': 'application/json'
+			},
+		};
+
+		request.post(options, function (err, httpResponse, body) {
 			if (err) {
 				console.error(err);
 				return res
 					.status(500)
-					.send(authUtil.successTrue(500, '파일을 저장하는 데 오류가 발생했습니다.', { error: err }));
+					.send(authUtil.successFalse(500, 'GPT에 요청을 보낼 수 없습니다.'));
 			}
+
+			if (body.error && body.error.code === 'model_not_found') {
+				return res
+					.status(400)
+					.send(authUtil.successFalse(400, '지정된 모델을 찾을 수 없거나 접근 권한이 없습니다.', { error: body.error }));
+			}
+
+			if (body.error && body.error.code === 'context_length_exceeded') {
+				return res
+					.status(400)
+					.send(authUtil.successFalse(400, '파일이 너무크거나 채팅기록이 너무 많습니다.', { error: body.error }));
+			}
+
+			const contents = body.choices ? body.choices.map(choice => {
+				return JSON.parse(choice.message.content);
+			}) : [];
 
 			res
 				.status(200)
-				.send(authUtil.successTrue(200, '파일업로드에 성공했습니다.', { FileURL: FILE_URL }));
+				.send(authUtil.successTrue(200, '파일업로드에 성공했습니다.', { FileURL: FILE_URL, Contents: contents }));
 		});
 	});
 });
